2f12f7f75fe789a663f66d42aac4ad3b866c490b
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2021-08-06 17:25:22 +0200

    ##VERSION OK 1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Header change background
 
 window.onscroll = function() {
-  if (document.documentElement.scrollTop > 80) {
-    document.getElementById("header").style.background="white";
+  if (document.documentElement.scrollTop > 150) {
+    document.getElementById("header").style.background="rgba(255,255,255,.7)";
     document.getElementById("desktop-left-title").style.color="black";
     document.getElementById("desktop-center-link").style.color="black";
   } else {
